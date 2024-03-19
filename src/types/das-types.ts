5b7ed1e5cbef56a11e732bde61d9a4bf3c546159
Chris Hagedorn <1844858+ChrisHagedorn@users.noreply.github.com>
2024-03-19 17:58:11 -0400

    Added getRwaAssetByMint to RpcClient

--- src/types/das-types.ts
+++ src/types/das-types.ts
@@ -9,279 +9,287 @@ import {
   RoyaltyModel,
   UseMethods,
 } from "./enums";
+import { FullAccount } from "./types";
 
-export namespace DAS { 
-// getAssetsByOwner //
-export interface AssetsByOwnerRequest {
-  ownerAddress: string;
-  page: number;
-  limit?: number;
-  before?: string;
-  after?: string;
-  displayOptions?: DisplayOptions;
-  sortBy?: AssetSortingRequest;
-}
+export namespace DAS {
+  // getAssetsByOwner //
+  export interface AssetsByOwnerRequest {
+    ownerAddress: string;
+    page: number;
+    limit?: number;
+    before?: string;
+    after?: string;
+    displayOptions?: DisplayOptions;
+    sortBy?: AssetSortingRequest;
+  }
 
-// getAssetsByCreator //
-export type AssetsByCreatorRequest = {
-  creatorAddress: string;
-  page: number;
-  onlyVerified?: boolean;
-  limit?: number;
-  before?: string;
-  after?: string;
-  displayOptions?: DisplayOptions;
-  sortBy?: AssetSortingRequest;
-};
+  // getAssetsByCreator //
+  export type AssetsByCreatorRequest = {
+    creatorAddress: string;
+    page: number;
+    onlyVerified?: boolean;
+    limit?: number;
+    before?: string;
+    after?: string;
+    displayOptions?: DisplayOptions;
+    sortBy?: AssetSortingRequest;
+  };
 
-export type GetAssetBatchRequest = { 
-  ids: Array<string>;
-  displayOptions?: GetAssetDisplayOptions;
-}
+  export type GetAssetBatchRequest = {
+    ids: Array<string>;
+    displayOptions?: GetAssetDisplayOptions;
+  }
 
-// getAssetsByGroup //
-export type AssetsByGroupRequest = {
-  groupValue: string;
-  groupKey: string;
-  page: number;
-  limit?: number;
-  before?: string;
-  after?: string;
-  displayOptions?: DisplayOptions;
-  sortBy?: AssetSortingRequest;
-};
-export type GetAssetsBatchRequest = {
-  ids: string[];
-};
+  // getAssetsByGroup //
+  export type AssetsByGroupRequest = {
+    groupValue: string;
+    groupKey: string;
+    page: number;
+    limit?: number;
+    before?: string;
+    after?: string;
+    displayOptions?: DisplayOptions;
+    sortBy?: AssetSortingRequest;
+  };
+  export type GetAssetsBatchRequest = {
+    ids: string[];
+  };
 
-// searchAssets
-export interface SearchAssetsRequest {
-  page: number; // starts at 1
-  limit?: number;
-  before?: string;
-  after?: string;
-  creatorAddress?: string;
-  ownerAddress?: string;
-  jsonUri?: string;
-  grouping?: string[];
-  burnt?: boolean;
-  sortBy?: AssetSortingRequest;
-  frozen?: boolean;
-  supplyMint?: string;
-  supply?: number;
-  interface?: string;
-  delegate?: number;
-  ownerType?: OwnershipModel;
-  royaltyAmount?: number;
-  royaltyTarget?: string;
-  royaltyTargetType?: RoyaltyModel;
-  compressible?: boolean;
-  compressed?: boolean;
-}
+  // searchAssets
+  export interface SearchAssetsRequest {
+    page: number; // starts at 1
+    limit?: number;
+    before?: string;
+    after?: string;
+    creatorAddress?: string;
+    ownerAddress?: string;
+    jsonUri?: string;
+    grouping?: string[];
+    burnt?: boolean;
+    sortBy?: AssetSortingRequest;
+    frozen?: boolean;
+    supplyMint?: string;
+    supply?: number;
+    interface?: string;
+    delegate?: number;
+    ownerType?: OwnershipModel;
+    royaltyAmount?: number;
+    royaltyTarget?: string;
+    royaltyTargetType?: RoyaltyModel;
+    compressible?: boolean;
+    compressed?: boolean;
+  }
 
-// getAssetsByAuthority
-export type AssetsByAuthorityRequest = {
-  authorityAddress: string;
-  page: number;
-  limit?: number;
-  before?: string;
-  after?: string;
-  displayOptions?: DisplayOptions;
-  sortBy?: AssetSortingRequest;
-};
-// getAsset
-export type GetAssetRequest = {
-  id: string;
-  displayOptions?: GetAssetDisplayOptions;
+  // getAssetsByAuthority
+  export type AssetsByAuthorityRequest = {
+    authorityAddress: string;
+    page: number;
+    limit?: number;
+    before?: string;
+    after?: string;
+    displayOptions?: DisplayOptions;
+    sortBy?: AssetSortingRequest;
+  };
+  // getAsset
+  export type GetAssetRequest = {
+    id: string;
+    displayOptions?: GetAssetDisplayOptions;
 
-};
-// getAssetProof
-export type GetAssetProofRequest = {
-  id: string;
-};
-// getSignaturesForAsset
-export type GetSignaturesForAssetRequest = {
-  id: string;
-  page: number;
-  limit?: number;
-  before?: string;
-  after?: string;
-};
+  };
+  // getRwaAsset
+  export type GetRwaAssetRequest = {
+    id: string;
+  };
+  // getAssetProof
+  export type GetAssetProofRequest = {
+    id: string;
+  };
+  // getSignaturesForAsset
+  export type GetSignaturesForAssetRequest = {
+    id: string;
+    page: number;
+    limit?: number;
+    before?: string;
+    after?: string;
+  };
 
-// Sorting on response
-export interface AssetSorting {
-  sort_by: AssetSortBy;
-  sort_direction: AssetSortDirection;
-}
-// Sorting on request (camelCase)
-export type AssetSortingRequest = {
-  sortBy: AssetSortBy;
-  sortDirection: AssetSortDirection;
-};
-// Asset Response
-export type GetAssetResponse = {
-  interface: Interface; // enum
-  id: string;
-  content?: Content;
-  authorities?: Authorities[];
-  compression?: Compression;
-  grouping?: Grouping[];
-  royalty?: Royalty;
-  ownership: Ownership;
-  creators?: Creators[];
-  uses?: Uses;
-  supply?: Supply;
-  mutable: boolean;
-  burnt: boolean;
-};
+  // Sorting on response
+  export interface AssetSorting {
+    sort_by: AssetSortBy;
+    sort_direction: AssetSortDirection;
+  }
+  // Sorting on request (camelCase)
+  export type AssetSortingRequest = {
+    sortBy: AssetSortBy;
+    sortDirection: AssetSortDirection;
+  };
+  // Asset Response
+  export type GetAssetResponse = {
+    interface: Interface; // enum
+    id: string;
+    content?: Content;
+    authorities?: Authorities[];
+    compression?: Compression;
+    grouping?: Grouping[];
+    royalty?: Royalty;
+    ownership: Ownership;
+    creators?: Creators[];
+    uses?: Uses;
+    supply?: Supply;
+    mutable: boolean;
+    burnt: boolean;
+  };
+  // RWA Asset Response
+  export type GetRwaAssetResponse = {
+    items: FullAccount
+  };
+  export type GetAssetResponseList = {
+    grand_total?: boolean;
+    total: number;
+    limit: number;
+    page: number;
+    items: GetAssetResponse[];
+  };
+  export interface GetAssetProofResponse {
+    root: string;
+    proof: Array<string>;
+    node_index: number;
+    leaf: string;
+    tree_id: string;
+  }
+  export interface GetSignaturesForAssetResponse {
+    total: number;
+    limit: number;
+    page?: number;
+    before?: string;
+    after?: string;
+    items: Array<Array<string>>;
+  }
+  // DisplayOptions
 
-export type GetAssetResponseList = {
-  grand_total?: boolean;
-  total: number;
-  limit: number;
-  page: number;
-  items: GetAssetResponse[];
-};
-export interface GetAssetProofResponse {
-  root: string;
-  proof: Array<string>;
-  node_index: number;
-  leaf: string;
-  tree_id: string;
-}
-export interface GetSignaturesForAssetResponse {
-  total: number;
-  limit: number;
-  page?: number;
-  before?: string;
-  after?: string;
-  items: Array<Array<string>>;
-}
-// DisplayOptions
+  export type DisplayOptions = {
+    showUnverifiedCollections?: boolean,
+    showCollectionMetadata?: boolean,
+    showGrandTotal?: boolean
+  }
 
-export type DisplayOptions = {
-  showUnverifiedCollections?: boolean,
-  showCollectionMetadata?: boolean,
-  showGrandTotal?: boolean
-}
+  // Display options for getAssetBatch do not include grand_total.
+  export type GetAssetDisplayOptions = {
+    showUnverifiedCollections?: boolean,
+    showCollectionMetadata?: boolean,
+  }
 
-// Display options for getAssetBatch do not include grand_total.
-export type GetAssetDisplayOptions = { 
-  showUnverifiedCollections?: boolean,
-  showCollectionMetadata?: boolean,
-}
+  // Ownership --
+  export interface Ownership {
+    frozen: boolean;
+    delegated: boolean;
+    delegate?: string;
+    ownership_model: OwnershipModel; // enum
+    owner: string;
+  }
+  // Supply --
+  export interface Supply {
+    print_max_supply: number;
+    print_current_supply: number;
+    edition_nonce?: number;
+  }
+  // Uses --
+  export interface Uses {
+    use_method: UseMethods; // enum
+    remaining: number;
+    total: number;
+  }
+  // Creators --
+  export interface Creators {
+    address: string;
+    share: number;
+    verified: boolean;
+  }
+  // Royalty --
+  export interface Royalty {
+    royalty_model: RoyaltyModel;
+    target?: string;
+    percent: number;
+    basis_points: number;
+    primary_sale_happened: boolean;
+    locked: boolean;
+  }
+  // Grouping --
+  export interface Grouping {
+    group_key: string;
+    group_value: string;
+    verified?: boolean;
+    collection_metadata?: CollectionMetadata;
 
-// Ownership --
-export interface Ownership {
-  frozen: boolean;
-  delegated: boolean;
-  delegate?: string;
-  ownership_model: OwnershipModel; // enum
-  owner: string;
-}
-// Supply --
-export interface Supply {
-  print_max_supply: number;
-  print_current_supply: number;
-  edition_nonce?: number;
-}
-// Uses --
-export interface Uses {
-  use_method: UseMethods; // enum
-  remaining: number;
-  total: number;
-}
-// Creators --
-export interface Creators {
-  address: string;
-  share: number;
-  verified: boolean;
-}
-// Royalty --
-export interface Royalty {
-  royalty_model: RoyaltyModel;
-  target?: string;
-  percent: number;
-  basis_points: number;
-  primary_sale_happened: boolean;
-  locked: boolean;
-}
-// Grouping --
-export interface Grouping {
-  group_key: string;
-  group_value: string;
-  verified?: boolean;
-  collection_metadata?: CollectionMetadata;
+  }
+  export interface CollectionMetadata {
+    name?: string;
+    symbol?: string;
+    image?: string;
+    description?: string;
+    external_url?: string;
 
-}
-export interface CollectionMetadata { 
-  name?: string;
-  symbol?: string;
-  image?: string;
-  description?: string;
-  external_url?: string;
+  }
+  // Authorities --
+  export interface Authorities {
+    address: string;
+    scopes: Array<Scope>;
+  }
 
-}
-// Authorities --
-export interface Authorities {
-  address: string;
-  scopes: Array<Scope>;
-}
+  //Links
+  export type Links = {
+    external_url?: string;
+    image?: string;
+    animation_url?: string;
+    [Symbol.iterator](): Iterator<Links>;
+  };
 
-//Links
-export type Links = {
-  external_url?: string;
-  image?: string;
-  animation_url?: string;
-  [Symbol.iterator](): Iterator<Links>;
-};
+  // Content --
+  export interface Content {
+    $schema: string;
+    json_uri: string;
+    files?: Files;
+    metadata: Metadata;
+    links?: Links;
+  }
 
-// Content --
-export interface Content {
-  $schema: string;
-  json_uri: string;
-  files?: Files;
-  metadata: Metadata;
-  links?: Links;
-}
-
-// FILE --
-export interface File {
-  uri?: string;
-  mime?: string;
-  cdn_uri?: string;
-  quality?: FileQuality;
-  contexts?: Context[];
-  [Symbol.iterator](): Iterator<File>;
-}
-// FILES --
-export type Files = File[];
-// Quality/ File --
-export interface FileQuality {
-  schema: string;
-}
-// Metadata/ Content --
-export interface Metadata {
-  attributes?: Attribute[];
-  description: string;
-  name: string;
-  symbol: string;
-}
-// Attributes
-export interface Attribute {
-  value: string;
-  trait_type: string;
-}
-// Compression
-export interface Compression {
-  eligible: boolean;
-  compressed: boolean;
-  data_hash: string;
-  creator_hash: string;
-  asset_hash: string;
-  tree: string;
-  seq: number;
-  leaf_id: number;
-}
-// End of DAS
+  // FILE --
+  export interface File {
+    uri?: string;
+    mime?: string;
+    cdn_uri?: string;
+    quality?: FileQuality;
+    contexts?: Context[];
+    [Symbol.iterator](): Iterator<File>;
+  }
+  // FILES --
+  export type Files = File[];
+  // Quality/ File --
+  export interface FileQuality {
+    schema: string;
+  }
+  // Metadata/ Content --
+  export interface Metadata {
+    attributes?: Attribute[];
+    description: string;
+    name: string;
+    symbol: string;
+  }
+  // Attributes
+  export interface Attribute {
+    value: string;
+    trait_type: string;
+  }
+  // Compression
+  export interface Compression {
+    eligible: boolean;
+    compressed: boolean;
+    data_hash: string;
+    creator_hash: string;
+    asset_hash: string;
+    tree: string;
+    seq: number;
+    leaf_id: number;
+  }
+  // End of DAS
 }
