d5726965b53057e2eda7680cad5574a0e48ad905
Evan <96965321+0xIchigo@users.noreply.github.com>
2025-01-27 17:26:18 -0500

    feat(optimized_transaction): Improve Sending and Polling Logic (#163)
    * Improve pollTransactionConfirmation
    
    * Implement Better Smart Tx Sends
    
    * Fix Default Values For pollTransactionConfirmation

--- src/constants.ts
+++ src/constants.ts
@@ -2,5 +2,5 @@
  * URL for the SFDP (Solana Foundation Delegation Program) Rejects list.
  * This can be used with the validatorAcls parameter to exclude certain validators.
  */
-export const SFDP_REJECTS_URL = 'https://helius-docs.s3.us-east-2.amazonaws.com/sfdp_rejects.json';
-
+export const SFDP_REJECTS_URL =
+  'https://helius-docs.s3.us-east-2.amazonaws.com/sfdp_rejects.json';
