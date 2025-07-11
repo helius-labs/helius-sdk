83bb8df7c121ca8e8a1e561578c0a035a178ac38
Evan <evan@helius.xyz>
2025-07-11 13:30:51 -0400

    Update Jest Config

--- jest.config.ts
+++ jest.config.ts
@@ -3,13 +3,32 @@ import type { Config } from "jest";
 const config: Config = {
     preset: "ts-jest",
     testEnvironment: "node",
-    testMatch: ["**/tests/**/*.test.ts"],
+    roots: ["<rootDir>/src"],
+    testMatch: ["**/__tests__/**/*.ts", "**/*.test.ts"],
+    moduleFileExtensions: ["ts", "js", "json"],
+    transform: {
+        "^.+\\.ts$": "ts-jest"
+    },
+    globals: {
+        "ts-jest": {
+            tsconfig: "<rootDir>/tsconfig.json"
+        }
+    },
     clearMocks: true,
     verbose: true,
     collectCoverage: true,
     collectCoverageFrom: ["src/**/*.ts"],
     coverageDirectory: "coverage",
     coverageReporters: ["text", "lcov"],
-}
+    // At least 60% test coverage across all metrics
+    coverageThreshold: {
+        global: {
+            branches: 60,
+            functions: 60,
+            lines: 60,
+            statements: 60
+        }
+    }
+};
 
 export default config;
