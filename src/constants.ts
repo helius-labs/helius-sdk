ec7d62c0d08efeafaf03938b5df74355b50d8377
Evan <evan@helius.xyz>
2025-07-11 12:08:09 -0400

    Start From Scratch

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,3 @@
-import { PublicKey } from "@solana/web3.js";
-
 /**
  * URL for the SFDP (Solana Foundation Delegation Program) Rejects list.
  * This can be used with the validatorAcls parameter to exclude certain validators.
@@ -7,4 +5,4 @@ import { PublicKey } from "@solana/web3.js";
 export const SFDP_REJECTS_URL =
   'https://helius-docs.s3.us-east-2.amazonaws.com/sfdp_rejects.json';
 
-export const HELIUS_VALIDATOR_PUBKEY = new PublicKey('he1iusunGwqrNtafDtLdhsUQDFvo13z9sUa36PauBtk');
+export const HELIUS_VALIDATOR_PUBKEY = 'he1iusunGwqrNtafDtLdhsUQDFvo13z9sUa36PauBtk';
